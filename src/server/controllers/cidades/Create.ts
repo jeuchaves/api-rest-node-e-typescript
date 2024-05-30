7b01f9b23342ad6623b6f8a3254b94e5aecc307c
Jeú Chaves <jeuchaveslima@gmail.com>
2024-05-30 18:11:20 -0400

    feat: add validator schema

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,32 +1,27 @@
-import { ObjectSchema, ValidationError, object, string } from 'yup';
-import { StatusCodes } from 'http-status-codes';
+import { object, string } from 'yup';
 import { Request, Response } from 'express';
+import { validation } from '../../shared/middleware';
 
 interface ICidade {
     nome: string;
     estado: string;
 }
 
-const bodyValidation: ObjectSchema<ICidade> = object({
-    nome: string().required().min(3),
-    estado: string().required().min(3),
+/* interface IFilter {
+    filter: string;
+} */
+
+export const createValidation = validation({
+    body: object({
+        nome: string().required().min(3),
+        estado: string().required().min(3),
+    }),
+    query: object({
+        filter: string().required().min(3),
+    }),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-    let validateData: ICidade | undefined = undefined;
-    try {
-        await bodyValidation.validate(req.body, { abortEarly: false });
-    } catch (err) {
-        const yupError = err as ValidationError;
-        const erros: Record<string, string> = {};
-
-        yupError.inner.forEach((error) => {
-            if (!error.path) return;
-            erros[error.path] = error.message;
-        });
-
-        return res.status(StatusCodes.BAD_REQUEST).json({ errors: erros });
-    }
-    console.log(req.body.nome);
+    console.log(req.body);
     return res.send('Create!');
 };
