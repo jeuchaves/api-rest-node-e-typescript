60d18d40ded5a2e710c5a965b2cbbb0779549900
Jeú Chaves <jeuchaveslima@gmail.com>
2024-05-31 15:14:56 -0400

    feat: add crud for cities

---
+++ src/server/controllers/cidades/UpdateById.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { number, object, string } from 'yup';
+
+import { validation } from '../../shared/middleware';
+
+interface IParamProps {
+    id?: number;
+}
+interface IBodyProps {
+    nome: string;
+}
+
+export const updateByIdValidation = validation((getSchema) => ({
+    params: getSchema<IParamProps>(
+        object({
+            id: number().moreThan(0).required().integer(),
+        })
+    ),
+    body: getSchema<IBodyProps>(
+        object({
+            nome: string().required().min(3),
+        })
+    ),
+}));
+
+export const updateById = async (
+    req: Request<IParamProps, {}, IBodyProps>,
+    res: Response
+) => {
+    console.log(req.params);
+    console.log(req.body);
+    return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send('Não implementado');
+};
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,15 +8,30 @@ router.get('/', (_, res) => {
     return res.send('Olá, DEV!');
 });
 
-router.post(
-    '/cidades',
-    CidadesController.createValidation,
-    CidadesController.create
-);
 router.get(
     '/cidades',
     CidadesController.getAllValidation,
     CidadesController.getAll
+);
+router.get(
+    '/cidades/:id',
+    CidadesController.getByIdValidation,
+    CidadesController.getById
+);
+router.put(
+    '/cidades/:id',
+    CidadesController.updateByIdValidation,
+    CidadesController.updateById
+);
+router.delete(
+    '/cidades/:id',
+    CidadesController.deleteByIdValidation,
+    CidadesController.deleteById
+);
+router.post(
+    '/cidades',
+    CidadesController.createValidation,
+    CidadesController.create
 );
 
 export { router };
