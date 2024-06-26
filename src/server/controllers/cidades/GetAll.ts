673efa47fcd987e5f118025e13c3089b029a8c73
Jeú Chaves <jeuchaveslima@gmail.com>
2024-06-26 12:40:02 -0400

    fix: Fix NaN value for id

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -31,7 +31,7 @@ export const getAll = async (
         req.query.page || 1,
         req.query.limit || 10,
         req.query.filter || '',
-        Number(req.query.id)
+        Number(req.query.id || 0)
     );
     const count = await CidadesProvider.count(req.query.filter);
 
